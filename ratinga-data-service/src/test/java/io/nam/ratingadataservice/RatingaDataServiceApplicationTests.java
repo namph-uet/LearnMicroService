efc0002b528e05c60ab5575534a166a3f6933574
Phạm Hoàng Nam <hoangnamuet.vnu@gmail.com>
2020-01-09 01:05:38 +0800

    created prj

---
+++ ratinga-data-service/src/test/java/io/nam/ratingadataservice/RatingaDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nam.ratingadataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingaDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
